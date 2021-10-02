ec592ecca55999b14f36b57dd2eca20d9cd65014
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-10-02 00:17:01 -0300

    Adicionado Swagger/OpenApi a aplicaçao

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	const APP_NAME = process.env.npm_package_name
+	const APP_VERSION = process.env.npm_package_version
+
+	const options = new DocumentBuilder()
+		.setTitle(APP_NAME)
+		.setDescription("BlockHub Project Manager")
+		.setVersion(APP_VERSION)
+		.build()
+	const document = SwaggerModule.createDocument(app, options)
+	SwaggerModule.setup('swagger', app, document)
+
+	await app.listen(3000)
 }
-bootstrap();
+bootstrap()

c2e99b76a53e878ca8fa69d66c60e9ecc4eaee12
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-10-02 11:10:03 -0300

    Corrigido configuraçao para ser compativel com modulos nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+const config = require("./sequelize/app/config/config")
+
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: config.dialect,
+      host: config.host,
+      port: config.port,
+      username: config.username,
+      password: config.password,
+      database: config.database,
+	  storage: config.storage,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/sequelize/app/config/config.ts
+++ src/sequelize/app/config/config.ts
@@ -1,8 +1,14 @@
 import * as dotenv from 'dotenv'
-import * as path from 'path';
-dotenv.config();
+import * as path from 'path'
+
+dotenv.config()
 
 module.exports = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE,
 	dialect: process.env.DB_DIALECT || 'sqlite',
     storage: process.env.DB_STORAGE_FILE || path.join('database', 'database.sqlite')
 }

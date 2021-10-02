4af446b5bfbea9a9c6794065b00377fb7075e866
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-10-02 14:26:05 -0300

    Corrigido Definiçoes do swagger do corpo da requisiçao de usuario (POST /usuario)

--- src/usuario/dtos/createUsuario.dto.ts
+++ src/usuario/dtos/createUsuario.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateUsuarioDto {
+    @ApiProperty()
     login: string
+    @ApiProperty()
     senha: string
 }
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,32 +1,36 @@
 import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
-import { ApiParam } from '@nestjs/swagger';
+import { ApiBody, ApiParam, ApiProperty } from '@nestjs/swagger';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dtos/createUsuario.dto';
 
 @Controller('usuario')
 export class UsuarioController {
-    constructor(private readonly usuarioService: UsuarioService) {}
+	constructor(private readonly usuarioService: UsuarioService) {}
 
-    @Get()
-    findAll(){
-        return this.usuarioService.findAll()
-    }
+	@Get()
+	findAll(){
+		return this.usuarioService.findAll()
+	}
 
-    @Get(':id')
-    @ApiParam({name: 'id', required: true, description: 'Identificador numérico do usuário na base de dados', schema: { type: 'integer'}})
-    async findOne(@Param('id') id: number){
-        const usuario = await this.usuarioService.findOne(id)
+	@Get(':id')
+	@ApiParam({name: 'id', required: true, description: 'Identificador numérico do usuário na base de dados', schema: { type: 'integer'}})
+	async findOne(@Param('id') id: number){
+		const usuario = await this.usuarioService.findOne(id)
 
-        if(!usuario){
-            throw new NotFoundException('User not found!')
-        }
+		if(!usuario){
+			throw new NotFoundException('User not found!')
+		}
 
-        return usuario
-    }
+		return usuario
+	}
 
-    @Post()
-    async create(@Body() createUsuarioDto: CreateUsuarioDto){
-
-    }
-    
+	@Post()
+	@ApiBody({
+		type: CreateUsuarioDto,
+		description: 'Usuário da Api'
+	})
+	async create(@Body() usuarioDto: CreateUsuarioDto){
+		return this.usuarioService.createUser(usuarioDto)
+	}
+	
 }

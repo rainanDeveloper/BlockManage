5c5ca62794e856c058d6439d02dd2ce55353f37e
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-10-04 06:19:23 -0300

    Criado model de projetos

---
+++ src/projetos/projetos.model.ts
@@ -0,0 +1,33 @@
+import { AutoIncrement, Model, Column, PrimaryKey, Table } from "sequelize-typescript"
+
+@Table({tableName:'projeto'})
+export class Projeto extends Model {
+
+	@PrimaryKey
+	@AutoIncrement
+	@Column
+	id: number
+
+	@Column
+	nome: string
+	
+	@Column
+	descricao: string
+	
+	@Column
+	inicio: Date
+	
+	@Column
+	fim: Date
+
+	@Column({
+		field: 'created_at'
+	})
+	createdAt: Date
+
+	@Column({
+		field: 'updated_at'
+	})
+	updatedAt: Date
+
+}

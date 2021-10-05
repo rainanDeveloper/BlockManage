a5ff9f439a7c6648dc40d685fdc031d7f16d102e
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-10-05 20:22:47 -0300

    Criado model de ProjetoColaborador

--- src/projeto-colaborador/projeto-colaborador.model.ts
+++ src/projeto-colaborador/projeto-colaborador.model.ts
@@ -1,3 +1,43 @@
-export class ProjetoColaborador {
+import { AutoIncrement, Model, Column, PrimaryKey, Table, ForeignKey, BelongsTo } from "sequelize-typescript"
+import { Colaborador } from "src/colaboradores/colaboradores.model";
+import { Projeto } from "src/projetos/projetos.model";
+
+@Table({tableName:'projeto_colaborador'})
+export class ProjetoColaborador extends Model {
+
+	@PrimaryKey
+	@AutoIncrement
+	@Column
+	id: number
     
+    @BelongsTo(() => Projeto)
+    projeto: Projeto;
+
+    @ForeignKey(() => Projeto)
+	@Column
+	projetoId: number
+
+    @BelongsTo(() => Colaborador)
+    colaborador: Colaborador
+
+    @ForeignKey(() => Colaborador)
+	@Column
+    colaboradorId: number
+    
+	@Column
+    inicio: Date
+    
+	@Column
+    fim: Date
+
+    @Column({
+		field: 'created_at'
+	})
+	createdAt: Date
+
+	@Column({
+		field: 'updated_at'
+	})
+	upDatedAt: Date
+
 }
